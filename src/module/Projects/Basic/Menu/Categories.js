770c92c62a8e0ee706b96d92c5539b81c2af7123
LalitKushwah <lalitkushwah52525@gmail.com>
2021-03-28 12:32:38 +0530

    remove btn-container from categories

--- src/module/Projects/Basic/Menu/Categories.js
+++ src/module/Projects/Basic/Menu/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = ({ filterItems, categories }) => {
     console.log(categories);
     return (
-        <div className="btn-container">
+        <div>
             {
                 categories.map(category => {
                     return (<button className="filter-btn" onClick={() => filterItems(category)}>{category}</button>)
